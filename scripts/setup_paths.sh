f4641cc372e5ded5b7b36850defdeda2e5ecc835
usman1515 <usmansiddique1515@gmail.com>
2022-06-14 01:23:05 +0500

    added export before paths in script setup_venv

--- scripts/setup_paths.sh
+++ scripts/setup_paths.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # ------------------------------ add folder compiler file path here
-DIR_COMPILER    = ${OPENTCAM_ROOT}/compiler
+export DIR_COMPILER    = ${OPENTCAM_ROOT}/compiler
 
 # --------------- source code
-DIR_COMP_MAIN   = ${DIR_COMPILER}/main.py
+export DIR_COMP_MAIN   = ${DIR_COMPILER}/main.py
+
 # --------------- tests
 
 # ------------------------------ add folder scripts file path here
-DIR_SCRIPTS     = ${OPENTCAM_ROOT}/scripts
-SETUP_PATHS     = ${OPENTCAM_ROOT}/scripts/setup_paths.sh
-SETUP_VENV      = ${OPENTCAM_ROOT}/scripts/setup_venv.sh
+export DIR_SCRIPTS     = ${OPENTCAM_ROOT}/scripts
+export SETUP_PATHS     = ${OPENTCAM_ROOT}/scripts/setup_paths.sh
+export SETUP_VENV      = ${OPENTCAM_ROOT}/scripts/setup_venv.sh
 
 # ------------------------------ add folder docs file path here
-DIR_DOCS        = ${OPENTCAM_ROOT}/docs
+export DIR_DOCS        = ${OPENTCAM_ROOT}/docs
 
 # ------------------------------ add folder images file path here
-DIR_IMAGES      = ${OPENTCAM_ROOT}/images
+export DIR_IMAGES      = ${OPENTCAM_ROOT}/images
 
