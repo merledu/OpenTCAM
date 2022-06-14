2de70bb956d4f1af89aac1acd82a5511c84a56c1
usman1515 <usmansiddique1515@gmail.com>
2022-06-14 23:28:54 +0500

    created py script tableMapping with class tableMapping

---
+++ compiler/src/tableMapping.py
@@ -0,0 +1,17 @@
+import logging
+import yaml
+import json
+
+class tableMapping:
+    # ----------------------------------------------------------------- Variables
+    def __init__(self):
+        # public vars
+        # protected vars
+        # private vars
+        # 
+        # # logging config
+        logging.basicConfig(level=logging.DEBUG, filename='../../logs/tableMapping.log',
+                            format='%(asctime)s | %(filename)s | %(funcName)s | %(levelname)s | %(lineno)d | %(message)s')
+    
+    # ----------------------------------------------------------------- Functions
+    
