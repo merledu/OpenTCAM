23ef9dd36dfec2b6bf66d98cecc3981703783be4
usman1515 <usmansiddique1515@gmail.com>
2022-07-09 01:45:40 +0500

    updated bash script setup_paths

--- scripts/setup_paths.sh
+++ scripts/setup_paths.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
 # ------------------------------ add folder compiler file path here
-export DIR_COMPILER         = ${OPENTCAM_ROOT}/compiler
+export DIR_COMPILER     = ${OPENTCAM_ROOT}/compiler
 
 # --------------- config files
-export DIR_COMP_CONFIGS     = ${DIR_COMPILER}/configs
+export DIR_COMP_CONFIGS = ${DIR_COMPILER}/configs
+
+# --------------- tcam table maps
+export DIR_COMP_LIB     = ${DIR_COMPILER}/lib
 
 # --------------- py source code
-export DIR_COMP_SRC         = ${DIR_COMPILER}/src
+export DIR_COMP_SRC     = ${DIR_COMPILER}/src
 
 # --------------- py tests
-export DIR_COMP_TESTS       = ${DIR_COMPILER}/tests
+export DIR_COMP_TESTS   = ${DIR_COMPILER}/tests
 
 # ------------------------------ add folder scripts file path here
-export DIR_SCRIPTS     = ${OPENTCAM_ROOT}/scripts
-export SETUP_PATHS     = ${OPENTCAM_ROOT}/scripts/setup_paths.sh
-export SETUP_VENV      = ${OPENTCAM_ROOT}/scripts/setup_venv.sh
+export DIR_SCRIPTS      = ${OPENTCAM_ROOT}/scripts
 
 # ------------------------------ add folder py venv file path here
 export DIR_VENV         = ${OPENTCAM_ROOT}/.pyVenvOpenTcam
 
 # ------------------------------ add folder docs file path here
-export DIR_DOCS        = ${OPENTCAM_ROOT}/docs
+export DIR_DOCS         = ${OPENTCAM_ROOT}/docs
 
 # ------------------------------ add folder images file path here
-export DIR_IMAGES      = ${OPENTCAM_ROOT}/images
+export DIR_IMAGES       = ${OPENTCAM_ROOT}/images
