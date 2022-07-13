6f87574377ecf753e657f8799a39c8720d8b5ae3
usman1515 <usmansiddique1515@gmail.com>
2022-07-13 11:41:25 +0500

    updated test testGetYAMLFilePath in py class TestTablemapping

--- compiler/tests/testTableMapping.py
+++ compiler/tests/testTableMapping.py
@@ -1,5 +1,6 @@
 from compiler.src.tableMapping import *
 from unittest import TestCase
+import pandas as pd
 import logging
 import yaml
 import glob
@@ -26,51 +27,14 @@ class TestTableMapping(TestCase):
     
     def testGetYAMLFilePath(self):
         # * ----- actual output
+        self.tm.getPrjDir()
         actualVal = self.tm.getYAMLFilePath()
         # * ----- expected output
         expectedVal = os.path.join(self.tm.prjWorkDir,'compiler/configs/tcamTables.yaml')
         # * ----- assertion
         self.assertEqual(actualVal,expectedVal,msg='MISMATCH in TCAM table YAML file path')
-    
-    
-    def testReadYAML(self):
-        # * ----- actual output
-        self.tm.getYAMLFilePath()
-        actualVal = self.tm.readYAML(self.tm.tcamTableConfigsFilePath)
-        # * ----- expected output
-        # * ----- assertion
-        self.assertEqual(type(actualVal),dict,msg='MISMATCH in reading TCAM table YAML config file')
-    
-    
-    def testGetTCAMConfig(self):
-        # * ----- actual output
-        self.tm.getYAMLFilePath()
-        self.tm.readYAML(self.tm.tcamTableConfigsFilePath)
-        actualVal = self.tm._tcamTableConfigs.keys()
-        with open('compiler/configs/tcamTables.yaml','r') as file:
-            fileData = yaml.full_load(file)
-        # * ----- expected output
-        expectedVal = fileData.keys()
-        # * ----- assertion
-        self.assertEqual(actualVal,expectedVal,msg='MISMATCH in total number of TCAM table YAML configs')
-    
-    
-    def testGetTCAMTableFilePath(self):
-        actualVal = list()
-        expectedVal = list()
-        # * ----- actual output
-        self.tm.getYAMLFilePath()
-        self.tm.readYAML(self.tm.tcamTableConfigsFilePath)
-        tempTcamConfig = list(self.tm._tcamTableConfigs.keys())
-        for i in range(len(tempTcamConfig)):
-            actualVal.append(self.tm.getTCAMTableFilePath(tempTcamConfig[i]))
-        # * ----- expected output
-        for file in glob.iglob('compiler/lib/*.xlsx',recursive=True):
-            expectedVal.append(file)
-        # * ----- assertion
-        actualVal.sort()
-        expectedVal.sort()
-        self.assertEqual(actualVal,expectedVal,msg='hello')
+
+
 
 # ===========================================================================================
 # ======================================== End Class ========================================
