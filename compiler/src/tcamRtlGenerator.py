ebf81196578b0221deb4d44bffc7d28796cbd112
usman1515 <usmansiddique1515@gmail.com>
2022-10-27 00:15:48 +0500

    created func getYAMLFilePath in py class TcamRtlGenerator

--- compiler/src/tcamRtlGenerator.py
+++ compiler/src/tcamRtlGenerator.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 import os
 
 # ===========================================================================================
@@ -10,13 +11,15 @@ class TcamRtlGenerator:
     def __init__(self):
         # * ------------------- public vars
         self.prjWorkDir = str()
+        self.tcamTableConfigsFilePath = str()
+        self.tcamTableConfigsFileName = str()
 
         # * ------------------- protected vars
         self._tcamTableConfigs  = dict()
 
         # * ------------------- private vars
         self.__tcamQueryStrLen  = int()
-\
+        
         # * logging config
         logging.basicConfig(level=logging.DEBUG, filename='./logs/tcamRtlGenerator.log',
                             format='%(asctime)s | %(filename)s | %(funcName)s | %(levelname)s | %(lineno)d | %(message)s')
@@ -33,7 +36,25 @@ class TcamRtlGenerator:
         logging.info('Project working dir: {:<s}'.format(self.prjWorkDir))
         printVerbose(verbose,'Project working dir: {:<s}'.format(self.prjWorkDir))
         return self.prjWorkDir
-
+    
+    
+    def getYAMLFilePath(self,verbose):
+        """
+        what does this func do ?
+        input args:
+        return val:
+        """
+        # * get tcamTables config file path
+        tempPath = os.path.join(self.prjWorkDir,'compiler/configs/tcamTables.yaml')
+        if os.path.isfile(tempPath) is True:
+            self.tcamTableConfigsFilePath = tempPath
+            self.tcamTableConfigsFileName = os.path.basename(tempPath)
+            logging.info('"FOUND": TCAM table config file path: {:<s}'.format(self.tcamTableConfigsFilePath))
+            printVerbose(verbose,'"FOUND": TCAM table config file path: {:<s}'.format(self.tcamTableConfigsFilePath))
+            return self.tcamTableConfigsFilePath
+        else:
+            logging.error('"NOT FOUND": TCAM table config file path: {:<s}'.format(self.tcamTableConfigsFilePath))
+            sys.exit('"NOT FOUND": TCAM table config file path: {:<s}'.format(self.tcamTableConfigsFilePath))
 
 
 
