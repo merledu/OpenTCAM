534bb4f90a8b165908a465fb93bc7b14cee6d9e6
usman1515 <usmansiddique1515@gmail.com>
2023-02-22 23:05:08 +0500

    created func ioPorts in py class andGate

--- compiler/src/andGate.py
+++ compiler/src/andGate.py
@@ -2,6 +2,7 @@
 List of all pip packages imported
 """
 
+import logging
 from migen import *
 from migen.fhdl.verilog import convert
 
@@ -23,10 +24,27 @@ class andGate(Module):
 
         :param _type_ ports: _description_
         :param _type_ dataWidth: _description_
+        :return _type_ verilogCode: _description_
         """
         self.numInputs = ports
         self.numInputWidth = dataWidth
         self.verilogCode = ""
+
+    def ioPorts(self, inputs, dataWidth):
+        """
+        _summary_
+
+        :param _type_ inputs: _description_
+        :param _type_ dataWidth: _description_
+        """
+        self.inputs = []
+        for inputs in range(inputs):
+            tempPort = Signal(dataWidth, name_override='in_data{}'.format(inputs))
+            self.inputs.append(tempPort)
+            logging.info('Created AND gate input port: {:>s}{:d}'.format('in_data', inputs))
+        self.outputs = Signal(dataWidth, name_override='out_data')
+        logging.info('Created AND gate output port: {:>s}{:d}'.format('in_data', inputs))
+
 
 
 # ===========================================================================================
