538f0c8266dfdca1964c240ac1ecdaedf6d86a21
usman1515 <usmansiddique1515@gmail.com>
2023-02-22 23:10:40 +0500

    created func logicBlock in py class andGate

--- compiler/src/andGate.py
+++ compiler/src/andGate.py
@@ -30,9 +30,14 @@ class andGate(Module):
         self.numInputWidth = dataWidth
         self.verilogCode = ""
 
+        # * setup IO ports
+        self.ioPorts(self, self.numInputs, self.numInputWidth)
+        # * generate module RTL
+        self.logicBlock(self)
+
     def ioPorts(self, inputs, dataWidth):
         """
-        _summary_
+        declare all the input output ports here
 
         :param _type_ inputs: _description_
         :param _type_ dataWidth: _description_
@@ -44,6 +49,14 @@ class andGate(Module):
             logging.info('Created AND gate input port: {:>s}{:d}'.format('in_data', inputs))
         self.outputs = Signal(dataWidth, name_override='out_data')
         logging.info('Created AND gate output port: {:>s}{:d}'.format('in_data', inputs))
+
+    def logicBlock(self):
+        """
+        Example of using reduce() to create an AND gate
+        """
+        self.comb += self.outputs.eq(reduce(lambda x, y: x & y, self.inputs))
+        logging.info('Generated AND gate logic')
+
 
 
 
