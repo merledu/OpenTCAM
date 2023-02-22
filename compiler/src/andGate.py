52e502e12653e809f6606dd2d1f09e32ca284756
usman1515 <usmansiddique1515@gmail.com>
2023-02-22 22:51:01 +0500

    created constructor for py class andGate

--- compiler/src/andGate.py
+++ compiler/src/andGate.py
@@ -1,5 +1,34 @@
+"""
+List of all pip packages imported
+"""
+
 from migen import *
 from migen.fhdl.verilog import convert
 
+# ===========================================================================================
+# ======================================= Begin Class =======================================
+# ===========================================================================================
+
 class andGate(Module):
-    pass
+    """
+    _summary_
+
+    """
+
+    # * ----------------------------------------------------------------- Variables
+    # Constructor: setup IO ports and logic here
+    def __init__(self, ports, dataWidth) -> None:
+        """
+        _summary_
+
+        :param _type_ ports: _description_
+        :param _type_ dataWidth: _description_
+        """
+        self.numInputs = ports
+        self.numInputWidth = dataWidth
+        self.verilogCode = ""
+
+
+# ===========================================================================================
+# ======================================== End Class ========================================
+# ===========================================================================================
