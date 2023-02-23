ae3bd68cc92ef08381bde8ea96020a05ce34a7d4
usman1515 <usmansiddique1515@gmail.com>
2023-02-24 00:13:34 +0500

    created py script tcamMemory7x64 with class tcamMemory7x64

---
+++ compiler/src/tcamMemory7x64.py
@@ -0,0 +1,22 @@
+"""
+List of all pip packages imported
+"""
+
+import logging
+from migen import *
+from migen.fhdl.verilog import convert
+
+# ===========================================================================================
+# ======================================= Begin Class =======================================
+# ===========================================================================================
+
+class tcamMemory7x64(Module):
+    """
+    _summary_
+
+    :param _type_ Module: _description_
+    """
+
+    # * ----------------------------------------------------------------- Variables
+    def __init__(self):
+        pass
