b90fb264f0dde3049d8b2954afa33cb8d965f7d4
usman1515 <usmansiddique1515@gmail.com>
2023-02-23 12:34:48 +0500

    created func ioPorts in py class priorityEncoder

--- compiler/src/priorityEncoder.py
+++ compiler/src/priorityEncoder.py
@@ -3,6 +3,8 @@ List of all pip packages imported
 """
 
 import logging
+import math
+import sys
 from migen import *
 from migen.fhdl.verilog import convert
 
@@ -17,15 +19,43 @@ class priorityEncoder(Module):
     :param _type_ Module: _description_
     :return _type_: _description_
     """
-    # * ----------------------------------------------------------------- Variables
+    # * ----------------------------------------------------------------- Functions
     def __init__(self, ports):
-        self.numInputs = ports
+        """
+        _summary_
+
+        :param _type_ ports: _description_
+        """        
+        self.inputWidth = ports
+        self.outputWidth = 0
         self.verilogCode = ''
 
         # * setup IO ports
-        # self.ioPorts()
+        self.ioPorts()
         # * generate block RTL
         # self.logicBlock()
+    
+    def ioPorts(self):
+        """
+        _summary_
+        """        
+        # * check if input port width is of 2^N
+        if math.floor(math.log2(self.inputWidth)) == math.ceil(math.log2(self.inputWidth)):
+            logging.info('"VALID": prioirty encoder input width {:d} is of 2^N'.format(self.inputWidth))
+            # * set output port width of log2(N)
+            self.outputWidth = int(math.log2(self.inputWidth))
+            logging.info('prioirty encoder output width: {:d}'.format(self.outputWidth))
+            # * create IO port objects
+            self.inputs = Signal(self.inputWidth, name_override='in_data')
+            logging.info('Created prioirty encoder input port: {:>s}'.format('in_data'))
+            self.outputs = Signal(self.outputWidth, name_override='out_data')
+            logging.info('Created prioirty encoder output port: {:>s}'.format('out_data'))
+            print(self.inputs)
+            print(self.outputs)
+        else:
+            print('num isnt 2^N', type(self.inputWidth))
+            logging.error('"INVALID": prioirty Encoder input width {:d} isnt of 2^N'.format(self.inputWidth))
+            sys.exit('"INVALID": prioirty encoder input width {:d} isnt of 2^N'.format(self.inputWidth))
 
 
 # ===========================================================================================
