deee0f334a078896c0089fbb1671e0e6d65fc1d2
usman1515 <usmansiddique1515@gmail.com>
2023-02-27 18:00:16 +0500

    created constructor for py class tcamMemTopWrapper

--- compiler/src/tcamMemTopWrapper.py
+++ compiler/src/tcamMemTopWrapper.py
@@ -1,5 +1,29 @@
+"""
+List of all pip packages imported
+"""
+
 from migen import *
 from migen.fhdl.verilog import convert
 
+# ===========================================================================================
+# ======================================= Begin Class =======================================
+# ===========================================================================================
+
 class tcamMemTopwrapper(Module):
-    pass
+    """
+    _summary_
+
+    :param _type_ Module: _description_
+    """
+
+    # * ----------------------------------------------------------------- Variables
+    def __init__(self):
+        """
+        _summary_
+        """
+        # * variables
+
+        # * setup IO ports
+        self.ioPorts()
+        # * generate block RTL
+        # self.logicBlock()
