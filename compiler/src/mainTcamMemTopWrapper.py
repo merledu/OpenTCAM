5f8870b89d0e5160045f7a313e223de1d30e1a95
usman1515 <usmansiddique1515@gmail.com>
2023-03-04 16:38:28 +0500

    created py file mainTcamMemTopWrapper

---
+++ compiler/src/mainTcamMemTopWrapper.py
@@ -0,0 +1,19 @@
+"""
+List of all pip packages imported
+"""
+from andGate import *
+from priorityEncoder import *
+from tcamMemBlock7x64 import *
+from tcamMemTopWrapper import *
+import os
+import logging
+import argparse
+os.system('clear')
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
